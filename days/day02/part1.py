6eaacb788d719362e806e318d225bb8b7587e25e
Garlug <epitropos@gmail.com>
2026-03-23 11:43:44 -0500

    Added day 2 part 1

---
+++ days/day02/part1.py
@@ -0,0 +1,31 @@
+"""
+Advent of Code 2025 — Day 1, Part 1
+https://adventofcode.com/2025/day/1
+"""
+
+from aoc.utils import parse
+
+def solve(data: str):
+    # Data is a single line. Split into an array.
+    values = parse(data)[0].split(',')
+    result = 0
+
+    for value in values:
+        minMax = value.split('-')
+        # Add one because max is inclusive
+        for i in range(int(minMax[0]), int(minMax[1]) + 1):
+            numStr = str(i)
+            length = len(numStr)
+
+            # If the number is odd it cannot contain duplicate numbers
+            if length % 2 == 1:
+                continue
+
+            # Get the substring numbers.
+            halfLength = int(length / 2)
+            left = numStr[0:halfLength]
+            right = numStr[halfLength:]
+
+            if left == right:
+                result += i
+    return result
---
+++ days/day02/part2.py
@@ -0,0 +1,10 @@
+"""
+Advent of Code 2025 — Day 1, Part 2
+https://adventofcode.com/2025/day/1
+"""
+
+from aoc.utils import parse
+
+def solve(data: str):
+    values = parse(data)
+    return None
